e54ca0f45f9b31aafe4c546e34719aa443521c75
exZon exZon <yashpatel5566789>
2020-08-31 20:16:50 +0530

    complete work

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+// API TMDB
+//54dcf0e8b3aa7d9ab890151980c8ba65
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDsEtb_bo-9sanRQEE5N_dhNS5WmG4Tzm0",
+    authDomain: "netflix-clone-6f976.firebaseapp.com",
+    databaseURL: "https://netflix-clone-6f976.firebaseio.com",
+    projectId: "netflix-clone-6f976",
+    storageBucket: "netflix-clone-6f976.appspot.com",
+    messagingSenderId: "915715597994",
+    appId: "1:915715597994:web:ba26a53efa29f777d552d3",
+    measurementId: "G-K7NW1RR6R4"
+  }; 
